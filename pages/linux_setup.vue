abc7417be7a8f74ded27da14a0b6f9146286d109
clemak27 <clemak27@mailbox.org>
2021-04-23 15:34:59 +0200

    update linux setup page

--- pages/linux_setup.vue
+++ pages/linux_setup.vue
@@ -3,7 +3,8 @@
     <h1> My Linux Setup </h1>
     <Gallery />
 
-    <h2> Some software I use </h2>
+    <a href="https://github.com/clemak27/linux_setup">dotfiles etc.</a>
+    <h4> Some software I use: </h4>
     <ul>
       <li> KDE + i3-gaps </li>
       <p> After trying out KDE and GNOME extensivly, I setteled on using KDE. This mostly came down to the KDE team providing better software and extension support than GNOME, also vastly improved styling options. Nowadays I'm also using i3wm together with KDE for that sweet tiling wm experience. </p>
@@ -16,11 +17,11 @@
       <li> firefox </li>
       <p> Best browser :) </p>
       <li> fzf </li>
-      <p> A great fuzzy finder that makes finding things easier, especiall ypaired with ripgrep. Great (n)vim support too. </p>
+      <p> A great fuzzy finder that makes finding things easier, especially paired with ripgrep. Great (n)vim support too. </p>
       <li> ranger </li>
       <p> A terminal file explorer with vim-linke keybinds. </p>
       <li> spotify-tui </li>
-      <p> I'm using spotify since I've my personal collection has grown way too large (especially when you want to put it on your smartphone as well). The spotify desktop app is not supported on linux. but the combination of spotifyd + spotify-tui definitly is :) </p>
+      <p> I'm using spotify since I've my personal collection has grown way too large (especially when you want to put it on your smartphone as well). The spotify desktop app is not (officially) supported on linux. but the combination of spotifyd + spotify-tui definitly is :) </p>
     </ul>
     <div id="btw">i use arch btw</div>
   </div>
