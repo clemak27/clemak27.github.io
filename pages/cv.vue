511773a22b53289eff22841d67a55ab4bd2492be
clemak27 <clemak27@mailbox.org>
2021-05-07 22:39:35 +0200

    add content

--- pages/cv.vue
+++ pages/cv.vue
@@ -1,10 +1,33 @@
 <template>
   <div class="cv">
     <h1> About me </h1>
-    <h2> Who am I? </h2>
-    <h2> Education </h2>
-    <h2> Work </h2>
-    <h2> Interests </h2>
+
+    <h3> Who am I? </h3>
+    My name is Clemens and I currently work as a Software Developer, mainly doing backend-stuff.
+
+    <h3> Education </h3>
+    I got a BSc in Computer Science at <a href="https://www.technikum-wien.at/en/">UAS Technikum Wien</a>. I also studied physics for a while until I figured out it wasn't for me.
+
+    <h3> Coding and stuff </h3>
+
+    I mainly use Java, Go and nodejs. I'm also interested in Kotlin and Rust, and I'm not afraid to try something new. I have a <a href="https://gitea.io/en-us/">gitea</a> server where I keep all my private repos, the code I feel confident enough to make public is on <a href="https://github.com/clemak27">GitHub</a>. I'm a Linux user <a href="https://archlinux.org/">btw</a> and do all of my editing in neovim and IntelliJ.
+
+    <h3> Music </h3>
+
+    I mostly listen to prog rock and metal. <a href="https://www.youtube.com/watch?v=DrDM6V08wcE">Here's a good example of music I like</a> (from my favorite artist :) )
+
+    <h3>Space</h3>
+    Space has always fascinated me since my childhood, both the scientific background and its exploration by humanity.
+
+    <h3>Sci-Fi</h3>
+    I enjoy watching science fiction movies (and playing scifi games) that are both entertaining and thought-provoking. Bladerunner 2049 is a good example for that (and also visually beautiful in my opinion). I'm also a big Star Wars fan (despite of Disney).
+
+    <h3>Formula 1</h3>
+    I'm not really into sports (and not very sporty myself), but I do enjoy watching F1. I have to admint I'm not the greatest Mercedes fan and more of a Verstappen guy.
+
+    <h3>Gaming</h3>
+    I do enjoy a good game from time to time. I used to play quite a lot on nintendo consoles when I was younger, nowadays I mostly enjoy indie and older titles on my PC. I also spend way too many hours playing Guild Wars when I was a teenager.
+
   </div>
 </template>
 
@@ -18,5 +41,9 @@ export default {
 .cv {
   padding: 3em;
   text-align: left;
+}
+
+h3 {
+  padding-top: 0.75em;
 }
 </style>
