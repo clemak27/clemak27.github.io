71e7e8d89d90922a5c12fd4816cca366bb7f70e0
clemak27 <clemak27@mailbox.org>
2021-05-07 21:41:03 +0200

    create new page

---
+++ pages/cv.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="cv">
+    <h1> About me </h1>
+    <h2> Who am I? </h2>
+    <h2> Education </h2>
+    <h2> Work </h2>
+    <h2> Interests </h2>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.cv {
+  padding: 3em;
+  text-align: left;
+}
+</style>
