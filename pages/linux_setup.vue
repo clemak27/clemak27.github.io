4ba7ceb1e5bec3b80330a4967e07a60951c5fb90
clemak27 <clemak27@mailbox.org>
2021-06-19 11:16:18 +0200

    update linux_setup screenshots

--- pages/linux_setup.vue
+++ pages/linux_setup.vue
@@ -6,8 +6,8 @@
     <a href="https://github.com/clemak27/linux_setup">dotfiles etc.</a>
     <h4> Some software I use: </h4>
     <ul>
-      <li> Plasma + Kröhnkite + latte-dock </li>
-      <p> After trying out Plasma and GNOME extensivly, I setteled on using Plasma. This mostly came down to Plasma + the KDE framework providing better software and extension support than GNOME, also vastly improved styling options. I also used i3wm for several months, but some issues with picom made me unfortunately reconsider it. Nowadays I'm using KDE together with Kröhnkite, a script that provides automatic tiling. I also use latte-dock, since it is vastly superior to the default panel. </p>
+      <li> i3-gaps </li>
+      <p> After using KDE extensivly, I setteled on using i3 as window manager. I like the automatic tiling and the configuration via dotfiles. </p>
       <li> alacritty </li>
       <p> Fast GPU accelerated terminal emulator. If used Konsole and Kitty before that and while I kinda miss my ligatures, I enjoy alacritty's speed and simplicity. </p>
       <li> neovim </li>
