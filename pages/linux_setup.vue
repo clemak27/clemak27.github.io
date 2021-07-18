de98e09dda16a8f201cad08c68372c7b947ebd7d
clemak27 <clemak27@mailbox.org>
2021-07-18 19:42:16 +0200

    update screenshot

--- pages/linux_setup.vue
+++ pages/linux_setup.vue
@@ -6,18 +6,12 @@
     <a href="https://github.com/clemak27/linux_setup">dotfiles etc.</a>
     <h4> Some software I use: </h4>
     <ul>
-      <li> i3-gaps </li>
-      <p> After using KDE extensivly, I setteled on using i3 as window manager. I like the automatic tiling and the configuration via dotfiles. </p>
+      <li> Plasma/i3-gaps </li>
       <li> alacritty </li>
-      <p> Fast GPU accelerated terminal emulator. If used Konsole and Kitty before that and while I kinda miss my ligatures, I enjoy alacritty's speed and simplicity. </p>
       <li> neovim </li>
-      <p> My editor of choice. With (way too many) plugins, I use it for editing all non java/kotlin/android projects I work on in my free time. </p>
       <li> firefox </li>
-      <p> Best browser :) </p>
       <li> fzf </li>
-      <p> A great fuzzy finder that makes finding things easier, especially paired with ripgrep. Great (n)vim support too. </p>
       <li> ranger </li>
-      <p> A terminal file explorer with vim-linke keybinds. </p>
     </ul>
     <div id="btw">i use arch btw</div>
   </div>
