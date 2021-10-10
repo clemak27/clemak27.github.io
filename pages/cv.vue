691aaeb4ddf800d0fb087e4b5cc72d0578e726fe
clemak27 <clemak27@mailbox.org>
2021-10-10 15:08:34 +0200

    update cv

--- pages/cv.vue
+++ pages/cv.vue
@@ -8,8 +8,8 @@
     <h3> Education </h3>
     I got a BSc in Computer Science at <a href="https://www.technikum-wien.at/en/">UAS Technikum Wien</a>. I also studied physics for a while until I figured out it wasn't for me.
 
-    <h3> Coding and stuff </h3>
-    I mainly use Java, Go and nodejs. I'm also interested in Kotlin and Rust, and I'm not afraid to try something new. I have a <a href="https://gitea.io/en-us/">gitea</a> server where I keep all my private repos, the code I feel confident enough to make public is on <a href="https://github.com/clemak27">GitHub</a>. I'm a Linux user <a href="https://archlinux.org/">btw</a> and do all of my editing in neovim and IntelliJ.
+    <h3> Coding </h3>
+    I mainly use Java, Go and nodejs. I'm also interested in Rust and write way too many shell scripts, and I'm not afraid to try something new. I have a <a href="https://gitea.io/en-us/">gitea</a> server where I keep all my private repos, the code I feel confident enough to make public is on <a href="https://github.com/clemak27">GitHub</a>. I'm a Linux user <a href="https://archlinux.org/">btw</a> and do all of my editing in neovim and IntelliJ.
 
     <h3> Music </h3>
     I mostly listen to progressive rock and metal. <a href="https://www.youtube.com/watch?v=DrDM6V08wcE">Here's a good example of music I like</a> (from my favorite artist :) ). Unfortunatly I don't play an instrument, but I'm hoping to learn guitar eventually.
@@ -20,8 +20,9 @@
     <h3>Sci-Fi</h3>
     I enjoy watching science fiction movies (and playing scifi games) that are both entertaining and thought-provoking. Bladerunner 2049 is a good example for that (and also visually beautiful in my opinion). I'm also a big Star Wars fan (despite of Disney).
 
-    <h3>Formula 1</h3>
-    I'm not really into sports (and not very sporty myself), but I do enjoy watching F1. Verstappen/Norris fan :)
+    <h3>Sports</h3>
+    I'm not really into sports (and not very sporty myself), but I do enjoy watching F1.
+    I also like walking/hiking if the situation presents itself.
 
     <h3>Gaming</h3>
     I do enjoy a good game from time to time. I used to play quite a lot on nintendo consoles when I was younger, nowadays I mostly enjoy indie and older titles on my PC. I also spend way too many hours playing Guild Wars when I was a teenager.
