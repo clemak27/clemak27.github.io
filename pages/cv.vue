20bd61af33657b11ce24bcbf89bcfb6d90830a99
clemak27 <clemak27@mailbox.org>
2022-04-28 16:18:14 +0200

    chore(cv): Update cv section

--- pages/cv.vue
+++ pages/cv.vue
@@ -5,16 +5,17 @@
         <h1> About me </h1>
 
         <h3> Who am I? </h3>
-        My name is Clemens and I currently work as a Software Developer, mainly doing backend-stuff.
+        My name is Clemens and I currently work as a Software Developer, maintaining and extending backend services.
 
         <h3> Education </h3>
         I got a BSc in Computer Science at <a href="https://www.technikum-wien.at/en/">UAS Technikum Wien</a>. I also studied physics for a while until I figured out it wasn't for me.
 
         <h3> Coding </h3>
-        I mainly use Java, Go and nodejs. I'm also interested in devops stuff and write way too many shell scripts, and I'm not afraid to try something new. I have a <a href="https://gitea.io/en-us/">gitea</a> server where I keep all my private repos, the code I feel confident enough to make public is on <a href="https://github.com/clemak27">GitHub</a>. I'm a Linux user and do all of my editing in neovim and IntelliJ.
+        I mainly use Java, Go and nodejs for programming, but I'm not afraid to try something new. There's also an interest in DevOps stuff and I have a server at home running NixOS. Automating all the boring stuff is also a worthwhile goal.
+        I have a <a href="https://gitea.io/en-us/">gitea</a> instance where I keep all my private repos, the code I feel confident enough to share is on <a href="https://github.com/clemak27">GitHub</a>. I'm a Linux user and do all of my editing in neovim and IntelliJ.
 
         <h3> Music </h3>
-        I mostly listen to progressive rock and metal. <a href="https://www.youtube.com/watch?v=DrDM6V08wcE">Here's a good example of music I like</a> (from my favorite artist :) ). I also picked up a guitar recently.
+        I mostly listen to progressive rock and other slightly weird music. <a href="https://www.youtube.com/watch?v=qFwcahcDzP4">Here's a good example of music I like</a>. I also picked up a guitar at the end of 2021 and have been practicing since then.
 
         <h3>Space</h3>
         Space has always fascinated me since my childhood, both the scientific background and its exploration by humanity.
