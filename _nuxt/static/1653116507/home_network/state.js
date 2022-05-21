092455500f48cc1027d0d3ab62f101a560cc0e70
clemak27 <clemak27@users.noreply.github.com>
2022-05-21 07:01:51 +0000

    deploy: 0434b944a79c39362524568476689ca5b0d69f14

---
+++ _nuxt/static/1653116507/home_network/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1653116507",layout:"default",error:a,serverRendered:true,routePath:"\u002Fhome_network",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
