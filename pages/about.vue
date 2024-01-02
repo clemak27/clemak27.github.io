5dfe7a85282f5a7a4c3ca145f741199d935fc291
clemak27 <clemak27@mailbox.org>
2024-01-02 11:05:14 +0100

    feat: nuxt3 rewrite

---
+++ pages/about.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1 class="text-2xl pb-3">
+      About this page
+    </h1>
+    <p>
+      This site was created by me, using
+      <a href="https://nuxt.com/" class="link">nuxt.js</a>.
+    </p>
+
+    <p>
+      <a href="https://icons8.com/icon/123742/home-page" class="link">Favicon</a>
+      by
+      <a href="https://icons8.com" class="link">Icons8</a>.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+p {
+  padding: 0 0 1rem 0;
+}
+</style>
