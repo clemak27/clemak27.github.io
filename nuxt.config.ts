c1e52f86a3f51a955c4d8e62834d2f29ca850314
clemak27 <clemak27@mailbox.org>
2024-08-12 21:32:22 +0200

    fix: update dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,15 +1,19 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+
   modules: [
     '@pinia/nuxt',
     // ["@nuxtjs/eslint-module", { lintOnStart: false }],
     '@nuxtjs/tailwindcss',
   ],
+
   app: {
     head: {
       title: 'clemak27',
       // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
     },
   },
+
+  compatibilityDate: '2024-08-12',
 })
