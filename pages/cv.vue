e84066bf2a1b26e29de06db7c9f3a94cdd8a6af7
clemak27 <clemak27@mailbox.org>
2024-12-06 13:57:04 +0100

    chore: fix linting issues

--- pages/cv.vue
+++ pages/cv.vue
@@ -39,13 +39,14 @@
       For reference, here is a good example what I like from one of my favourite artists:
     </p>
 
-    <iframe width="560"
-            height="315"
-            src="https://www.youtube-nocookie.com/embed/wYfQ1I-VV7M?si=Cp0BVcGKMKKmzU8G"
-            title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            allowfullscreen
-    ></iframe>
+    <iframe
+      width="560"
+      height="315"
+      src="https://www.youtube-nocookie.com/embed/wYfQ1I-VV7M?si=Cp0BVcGKMKKmzU8G"
+      title="YouTube video player" frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      allowfullscreen
+    />
 
     <h2 class="text-lg">
       Space
